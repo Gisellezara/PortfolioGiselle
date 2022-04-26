5471c7ffaf49c4d3eb8d2b6859dc4707f67bcf82
Giselle Guevara <giselleguevara@R2Hstudent.org>
2022-04-25 20:00:49 -0400

    edit description on projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -46,7 +46,7 @@ const Projects = () => {
               </div>
               <div className="column">
                 <h2 className="title">Hope Hacks: Planet Progress</h2>
-                <p className="text">This Hackathon project was elaborated by three individuals, excluding me. We use API to get information on the air quality and how many RV charging stations are within a one-mile radius of the city you put in. I was responsible for the page’s design and how we wanted to display our information. I use gride containers to separate my sections, place each area where it needs to go and add a media query to make the page responsive.</p>
+                <p className="text">This Hackathon project was elaborated by my team we use API to get information on the air quality and how many RV charging stations are within a one-mile radius of the city you put in. I was responsible for the page’s design and how we wanted to display our information. I use gride containers to separate my sections, place each area where it needs to go and add a media query to make the page responsive.</p>
                 <div className="btncont">
                 <a
                   class="btn"
@@ -109,7 +109,7 @@ const Projects = () => {
               </div>
               <div className="column">
                 <h2 className="title">Eccommerce: Gigi Designs </h2>
-                <p className="text">I designed this project based on my collection of cups. Gigi Designs is a website where customers can go and buy custom-made cups or exclusive cups. I created this project on React and had to develop an MYSQL database that holds all of my product’s information. I then used Express to get my data from the database and render it to my produces page. I ended up using SASS for my CSS, making it easier to locate things.</p>
+                <p className="text">Gigi Designs is a website where customers can go and buy custom-made cups or exclusive cups. I created this project on React and had to develop an MYSQL database that holds all of my product’s information. I then used Express to get my data from the database and render it to my produces page and used SASS for my CSS, making it easier to locate things.</p>
                 <div className="btncont">
                 <a class="btn" href="https://github.com/Gisellezara/ecomGigi.git" role="button">
                   View GitHub
